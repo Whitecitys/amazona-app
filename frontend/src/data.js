940b39c7a7f7bdbdced366c77c7feae8bd979733
Whitecitys <Whitecitys10@gmail.com>
2021-12-19 00:02:28 +0700

    share code

---
+++ frontend/src/data.js
@@ -0,0 +1,73 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:'Nike Slimn Shirt 1',
+            catagory:'Shirts',
+            image:'/images/p1.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4.5,
+            numReview:10,
+            description:'hign quality product'
+        },
+        {
+            _id:'2',
+            name:'Adidas fit Shirt',
+            catagory:'Shirts',
+            image:'/images/p2.jpg',
+            price:120,
+            brand:'Adidas',
+            rating:4.0,
+            numReview:10,
+            description:'hign quality product'
+        },
+        {
+            _id:'3',
+            name:'Lacoste Free Shirt 3',
+            catagory:'Shirts',
+            image:'/images/p1.jpg',
+            price:220,
+            brand:'Lacoste',
+            rating:4.8,
+            numReview:1,
+            description:'hign quality product'
+        },
+        {
+            _id:'4',
+            name:'Nike slim Pants',
+            catagory:'Shirts',
+            image:'/images/p1.jpg',
+            price:78,
+            brand:'Nike',
+            rating:4.5,
+            numReview:14,
+            description:'hign quality product'
+        },
+        {
+            _id:'5',
+            name:'Puma Slimn Pant',
+            catagory:'Pants',
+            image:'/images/p1.jpg',
+            price:65,
+            brand:'Puma',
+            rating:4.5,
+            numReview:10,
+            description:'hign quality product'
+        },
+        {
+            _id:'6',
+            name:'Adidas Free Pant',
+            catagory:'Pants',
+            image:'/images/p1.jpg',
+            price:139,
+            brand:'Adidas',
+            rating:4.5,
+            numReview:15,
+            description:'hign quality product'
+        },
+        
+    ]
+};
+
+export default data;
